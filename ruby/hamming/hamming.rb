1ee73d108c46fd5b7f00ccbc71c32696e8f2e610
Michael Palhidai <mpalhidai@gmail.com>
2018-03-12 07:57:54 -0700

    Solved Ruby Exercise 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,14 @@ class Hamming
   def Hamming.compute(first,second)
     if first.length == 0 || second.length == 0
       return 0
+    elsif first.length != second.length
+      raise ArgumentError
+    else
+      return first.chars.each_index.count{|idx| first[idx] != second[idx]}
     end
   end
+end
+
+module BookKeeping
+   VERSION = 3 # Where the version number matches the one in the test.
 end
