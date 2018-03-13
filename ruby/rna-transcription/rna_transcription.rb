681b003965d67a5892913d0a24c21d9aa062f74d
Michael Palhidai <mpalhidai@gmail.com>
2018-03-13 07:52:16 -0700

    Added to rna_trans

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,4 +1,8 @@
 class Complement
   def self.of_dna(myarg)
+    rnaHash = {'C'=>'G', 'G'=>'C', 'T'=>'A', 'A'=>'U'}
+    newstr = ''
+    myarg.chars.each {|letter| rnaHash[letter] != nil ? newstr += rnaHash[letter] : return newstr}
+    newstr
   end
 end
